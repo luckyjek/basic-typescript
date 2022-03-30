09101d8246dd5d2833d1fd210e6ffc9bb54c11b4
luckyjek <thxjek@gmail.com>
2022-03-30 21:43:21 +0900

    when use function type - spread, default, optional

--- 1-types/1-2-funciton.ts
+++ 1-types/1-2-funciton.ts
@@ -4,12 +4,12 @@
         return num1 + num2;
     }
 
-    //TypeScript ✨
+    // //TypeScript ✨
     function add(num1: number, num2: number): number{
         return num1 + num2;
     }
 
-    // JavaScript 💩
+    //JavaScript 💩
     function jsFetchNum(id) {
         // code ...
         // code ...
@@ -20,10 +20,10 @@
     }
 
     // TypeScript ✨
-    // function fetchNum(id: string):object {
-        //인자로는 string을 받아서 Promise를 return하는구나.
-        //그리고 이 Promise는 fetch가 완료된 다음에 숫자의 데이터를 
-        //return하겠구나라고 예상할 수가 있다.
+    // function fetchNum(id: string): object {
+        // 인자로는 string을 받아서 Promise를 return하는구나.
+        // 그리고 이 Promise는 fetch가 완료된 다음에 숫자의 데이터를 
+        // return하겠구나라고 예상할 수가 있다.
     function fetchNum(id: string):Promise<number> {
         // code ...
         // code ...
@@ -33,4 +33,30 @@
         });
     }
     
+
+    //JavaScript ✨ => TypeScript
+    //Optional parameter
+    function printName(firstName: string, lastName?:string){
+        console.log(firstName);
+        console.log(lastName);
+    }
+
+    printName('Steve', 'Jobs');
+    printName('Ellie');
+    printName('Anna',undefined);
+
+    //Default parameter
+    function printMessage(message: string = 'default message'){
+        console.log(message);
+    }
+    printMessage();
+
+    //Rest parameter
+    // function addNumbers(num1: number, ...rest: number[]): number{
+    function addNumbers(...numbers: number[]): number{
+        return numbers.reduce((a, b) => a + b);
+    }
+    console.log(addNumbers(1,2));
+    console.log(addNumbers(1,2,3,4));
+    console.log(addNumbers(1,2,3,4,5,0));
 }
